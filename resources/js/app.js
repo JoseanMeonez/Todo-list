30ce2c5da28dbd147bca325aabd2c7da9d6d385e
Josean <joseanmeonez@gmail.com>
2022-12-27 12:25:16 -0600

    Changing some assets directories

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,10 @@
-import './bootstrap';
-import '../sass/app.scss'
+import './bootstrap.js';
+// import '../sass/app.scss'
+
+window.addEventListener('load', () => {
+	var add_todo = document.getElementById('add-todo');
+	
+	add_todo.onclick = function () {
+		console.log('Hello');
+	}
+})
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 	<div class="card m-3">
 		<div class="card-body">
-			<div class="input-group">
+			<div class="input-group shadow rounded">
 				<input type="text" class="form-control">
-				<input type="button" class="input-group-text btn btn-danger" value="Add Todo" style="background-color: orangered">
+				<input type="button" class="input-group-text btn text-white fw-bold btn-orangered" id="add-todo" value="Add Todo" >
 			</div>
 		</div>
 	</div>
