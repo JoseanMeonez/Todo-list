cb128757d6a00718ec139050b1796285283d57c7
Josean <joseanmeonez@gmail.com>
2022-12-26 20:29:08 -0600

    Adding the files for the first time.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="card m-3">
+		<div class="card-body">
+			<div class="input-group">
+				<input type="text" class="form-control">
+				<input type="button" class="input-group-text btn btn-danger" value="Add Todo" style="background-color: orangered">
+			</div>
+		</div>
+	</div>
+@endsection
