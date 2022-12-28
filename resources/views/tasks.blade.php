6973a7397fb0fa8d0156672ea48cc07cf79bf665
Josean <joseanmeonez@gmail.com>
2022-12-27 18:44:57 -0600

    Frontend tasks desing updates.

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,30 +1,23 @@
 @extends('welcome')
 @section('tasks')
-	<div class="list-group">
-		<div class="input-group">
-			<div class="list-group-item list-group-item-action" aria-current="true">
-				<div class="d-flex w-100 justify-content-between">
-					<p class="mb-1 task-text" id="tt-1">Some placeholder content in a paragraph.</p>
-					<input type="checkbox" class="check" onclick="$ch3ck3d('tt-1', this.getAttribute('id'))" id="2" value=0>
-				</div>
-				<small id="sch-">3 days ago</small>	
-			</div>
-		</div>
-		{{-- <a href="#" class="list-group-item list-group-item-action">
-			<div class="d-flex w-100 justify-content-between">
-				<h5 class="mb-1">List group item heading</h5>
-				<small class="text-muted">3 days ago</small>
-			</div>
-			<p class="mb-1">Some placeholder content in a paragraph.</p>
-			<small class="text-muted">And some muted small print.</small>
-		</a>
-		<a href="#" class="list-group-item list-group-item-action">
-			<div class="d-flex w-100 justify-content-between">
-				<h5 class="mb-1">List group item heading</h5>
-				<small class="text-muted">3 days ago</small>
-			</div>
-			<p class="mb-1">Some placeholder content in a paragraph.</p>
-			<small class="text-muted">And some muted small print.</small>
-		</a> --}}
-	</div>
+<ul class="list-group shadow">
+  <li class="list-group-item">
+		<input type="checkbox" class="form-check-input btn-check" onclick="$ch3ck3d('tt-1', this.getAttribute('id'))" id="2" value=0>
+		<label class="form-check-label btn btn-sm btn-outline-primary fs-6" for="2"><i class="bi bi-check-lg"></i></label>
+		<button type="button" class="input-group-text btn btn-sm btn-outline-success" title="Edit Task" id="add-todo">
+			<i class="bi bi-pencil-fill fs-6"></i>
+		</button>
+    <label class="form-check-label ms-2 task-text" id="tt-1">Some placeholder content in a paragraph.</label>
+    <label class="form-check-label ms-1 text-muted text-end" id="tt-1"> - Scheduled to 13-12-2022.</label>
+  </li>
+	<li class="list-group-item">
+		<input type="checkbox" class="form-check-input btn-check" onclick="$ch3ck3d('tt-1', this.getAttribute('id'))" id="2" value=0>
+		<label class="form-check-label btn btn-sm btn-outline-primary fs-6" for="2"><i class="bi bi-check-lg"></i></label>
+		<button type="button" class="input-group-text btn btn-sm btn-outline-success" title="Edit Task" id="add-todo">
+			<i class="bi bi-pencil-fill fs-6"></i>
+		</button>
+    <label class="form-check-label ms-2 task-text" id="tt-1">Some placeholder content in a paragraph.</label>
+    <label class="form-check-label ms-1 text-muted text-end" id="tt-1"> - Scheduled to 13-12-2022.</label>
+  </li>
+</ul>
 @endsection
